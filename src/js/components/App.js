2aff750f220181c956854a3266a71dda6a79c0a2
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-05-21 17:57:18 +0300

    Basic infrastructure :smiley_cat:

---
+++ src/js/components/App.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React from 'react';
+import { createStore } from 'redux';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello Redux</h1>
+    );
+  }
+}
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+ReactDOM.render(<App />, document.getElementById('main'))

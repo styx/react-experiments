82d5ce52d4ddb7fcd31cadd8cf51e7d503416845
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-05-22 09:27:59 +0300

    Add Todo toggle

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -34,7 +34,19 @@ export default class App extends React.Component {
 
         <ul>
           {todos.map(todo =>
-            <li key={todo.id}>
+            <li key={todo.id} onClick={() => {
+              store.dispatch({
+                type: 'TOGGLE_TODO',
+                id: todo.id
+              });
+            }}
+            style={{
+              textDecoration:
+                todo.completed ?
+                  'line-through' :
+                  'none'
+            }}
+            >
               {todo.text}
             </li>
           )}

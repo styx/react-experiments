bf793e88acb7282de00303f0496aacfc2f685c1e
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-05-28 09:24:08 +0300

    Use Connect from react-redux

--- src/js/components/VisibleTodoList.js
+++ src/js/components/VisibleTodoList.js
@@ -1,49 +1,38 @@
 'use strict';
 
 import React from 'react';
+import { connect } from 'react-redux';
 import TodoList from './TodoList';
 
-export default class VisibleTodoList extends React.Component {
-  static contextTypes = {
-    store: React.PropTypes.object.isRequired
-  }
-
-  componentDidMount() {
-    this.unsubscribe = this.context.store.subscribe(() =>
-      this.forceUpdate()
-    );
-  }
-
-  componentWillUnMount() {
-    this.unsubscribe();
-  }
-
-  getVisibleTodos(todos, filter) {
-    switch (filter) {
-      case 'SHOW_ACTIVE':
-        return todos.filter(t => !t.completed);
-      case 'SHOW_COMPLETED':
-        return todos.filter(t => t.completed);
-      case 'SHOW_ALL':
-      default:
-        return todos;
-    }
-  }
-
-  onTodoClick(id) {
-    this.context.store.dispatch({
-      type: 'TOGGLE_TODO',
-      id: id
-    });
-  }
-
-  render() {
-    const { store } = this.context;
-    let { todos, visibilityFilter } = store.getState();
-    let visibleTodos = this.getVisibleTodos(todos, visibilityFilter);
-
-    return (
-      <TodoList todos={visibleTodos} onTodoClick={this.onTodoClick.bind(this)}/>
-    );
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case 'SHOW_ACTIVE':
+      return todos.filter(t => !t.completed);
+    case 'SHOW_COMPLETED':
+      return todos.filter(t => t.completed);
+    case 'SHOW_ALL':
+    default:
+      return todos;
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    todos: getVisibleTodos(
+      state.todos, state.visibilityFilter
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: (id) => {
+      dispatch({
+        type: 'TOGGLE_TODO',
+        id: id
+      });
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);

cb44f77144cc34147722d8fb81c1420d2287c7fa
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-06-09 10:36:37 +0300

    Extract reducers

--- src/js/main.js
+++ src/js/main.js
@@ -6,53 +6,9 @@ import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { loadState, saveState } from './utils/localStorage';
 import throttle from 'lodash/throttle';
+import { todoApp } from './reducers';
 
 import App from './components/App';
-
-const todo = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      }
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
-      }
-
-      return {
-        ...state,
-        completed: !state.completed
-      }
-    default:
-      return state;
-  }
-};
-
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ]
-    case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action));
-    default:
-      return state;
-  }
-};
-
-const visibilityFilter = (state = 'SHOW_ALL', action) => {
-  switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
-      return action.filter;
-    default:
-      return state;
-  }
-};
 
 const render = () => {
   ReactDOM.render(
@@ -63,7 +19,6 @@ const render = () => {
   );
 };
 
-const todoApp = combineReducers({todos, visibilityFilter});
 const store = createStore(todoApp, loadState(),
   window.devToolsExtension ? window.devToolsExtension() : undefined
 );

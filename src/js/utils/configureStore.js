74a5f8a34c41d381fca8415249ae529d4a1357d5
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-06-14 20:42:08 +0300

    Add logger for dispatcher

--- src/js/utils/configureStore.js
+++ src/js/utils/configureStore.js
@@ -3,10 +3,28 @@ import { loadState, saveState } from './localStorage';
 import throttle from 'lodash/throttle';
 import todoApp from '../reducers';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+
+    return returnValue;
+  }
+};
+
 const configureStore = () => {
   const store = createStore(todoApp, loadState(),
     window.devToolsExtension ? window.devToolsExtension() : undefined
   );
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   store.subscribe(throttle(() => {
     saveState({

1634ac2b8e4e0e5fcc023466d92343433ce5a621
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-06-16 10:55:55 +0300

    Use redux-thunk instead of own

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -16,7 +16,7 @@ const receiveTodos = (filter, response) => ({
 
 export const fetchTodos = (filter) => (dispatch, getState) => {
   if (getIsFetching(getState(), filter)) {
-    return;
+    return Promise.resolve();
   }
   dispatch(requestTodos(filter));
 
--- src/js/reducers/createList.js
+++ src/js/reducers/createList.js
@@ -24,7 +24,7 @@ const createList = (filter) => {
       default:
         return state;
     }
-  }
+  };
 
   return combineReducers({
     ids,

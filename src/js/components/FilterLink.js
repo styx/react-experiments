fc1f4a96462a4d6521ec064a18798fdb11e24cec
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-06-25 21:23:42 +0300

    Fix typo

--- src/js/components/FilterLink.js
+++ src/js/components/FilterLink.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const FilterLink = (filter, children) => (
+const FilterLink = ({ filter, children }) => (
   <Link
     to={filter === 'all' ? '' : filter}
     activeStyle={{
